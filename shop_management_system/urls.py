8586165d0ba0c3928c2b29e223dd8ddb80e5c5ac
Fotsingboris <fotsingtchoupe1@gmail.com>
2026-07-30 12:33:50 +0100

    feat: configured tailwind css

--- shop_management_system/urls.py
+++ shop_management_system/urls.py
@@ -14,20 +14,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include, static
+from django.conf.urls import include
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 from shop_management_system import settings
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='auth/login.html'), name='login'),
     
-    path('', include('general.urls')),
+    path('general', include('general.urls')),
     path('sales/', include('sales.urls')),
     path('users/', include('users.urls')),
     path('products/', include('products.urls')),
     
-    # Media files
-    *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
